3c336abb6f34cdb05483b834e09c77ac6cb04adc
John Yarham <John.Yarham@red-gate.com>
2018-05-27 10:27:47 +0100

    use modal to create card

--- ClientApp/components/CardList.tsx
+++ ClientApp/components/CardList.tsx
@@ -2,9 +2,17 @@ import * as React from "react";
 import { Segment, Button } from "semantic-ui-react";
 import { ToDoCard } from "./ToDoCard";
 import "../css/CardList.css";
+import { CardModal } from "./CardModal";
+
+interface IList {
+    id: number;
+    name: string;
+};
 
 interface ICardListState {
     numCards: number;
+    cardNames: IList[];
+    idCount: number;
     newListName: string;
 }
 
@@ -19,21 +27,29 @@ export class CardList extends React.Component<ICardListProps, ICardListState> {
         super();
         this.state = {
             numCards: 0,
+            cardNames: [],
+            idCount: 0,
             newListName: ""
         }
     }
 
-    public addCard = () => {
-        this.setState((prevState) => ({ numCards: prevState.numCards + 1 }));
+    public addCard = (cardName: string) => {
+        this.setState((prevState: any) => ({
+            cardNames: [...prevState.cardNames, { id: prevState.idCount, name: cardName }],
+            idCount: prevState.idCount + 1
+        }));
     }
 
-    public populateCardsSegment = () => {
+    public createCards = () => {
         let cards: Object[] = [];
-        for (let i = 0; i < this.state.numCards; i++) {
-            cards.push(<ToDoCard key={i} />);
+        for (let i = 0; i < this.state.cardNames.length; i++) {
+
+            const id = this.state.cardNames[i].id;
+            cards.push(<ToDoCard key={id} />);
         }
+
         return cards;
-    }
+    };
 
     public addDeleteListButton = () => {
         if (this.state.numCards === 0) {
@@ -45,14 +61,14 @@ export class CardList extends React.Component<ICardListProps, ICardListState> {
         this.setState({ newListName: input });
     };
 
-    public handleKeyPress = (event: any) => {
+    public changeListNameOnKeyPress = (event: any) => {
         if (event.key === "Enter") {
             event.preventDefault();
             event.target.blur();
         }
     };
 
-    public handleOutsideClick = () => {
+    public changeListNameOnOutsideClick = () => {
         this.props.updateList(this.state.newListName);
     };
 
@@ -64,14 +80,14 @@ export class CardList extends React.Component<ICardListProps, ICardListState> {
                         className="list-name"
                         defaultValue={this.props.listName}
                         onChange={(event: any) => this.changeListName(event.target.value)}
-                        onKeyPress={this.handleKeyPress}
-                        onBlur={this.handleOutsideClick} />
+                        onKeyPress={this.changeListNameOnKeyPress}
+                        onBlur={this.changeListNameOnOutsideClick} />
                 </Segment>
                 <Segment className="cards-segment">
-                    {this.populateCardsSegment()}
+                    {this.createCards()}
                 </Segment>
                 <Segment className="button-segment">
-                    <Button compact primary onClick={this.addCard}>Add a card</Button>
+                    <CardModal addCard={(cardName) => this.addCard(cardName)}/>
                     {this.addDeleteListButton()}
                 </Segment>
             </Segment.Group>
--- ClientApp/components/CardModal.tsx
+++ ClientApp/components/CardModal.tsx
@@ -1,16 +1,60 @@
 import * as React from "react";
-import { Segment, Button } from "semantic-ui-react";
+import { Button, Modal, Form } from "semantic-ui-react"
+import "../css/cardModal.css";
 
-interface IButtonSegmentProps {
-    addCard(): void;
+interface ICardModalProps {
+    addCard(cardName: string): void;
 }
 
-export class ButtonSegment extends React.Component<IButtonSegmentProps, {}> {
+export class CardModal extends React.Component<ICardModalProps, {}> {
+    public state = {
+        modalOpen: false,
+        cardName: "",
+    }
+
+    public handleOpen = () => this.setState({ modalOpen: true });
+
+    public handleClose = () => {
+        this.props.addCard(this.state.cardName);
+        this.setState({ modalOpen: false, cardName: "" });
+    };
+
+    public handleCancel = () => {
+        this.setState({ modalOpen: false });
+    };
+
+    public handleInput = (input: string) => {
+        this.setState({ cardName: input });
+    };
+
+    public handleKeyPress = (event: any) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            this.handleClose();
+        }
+    };
+
     public render() {
         return (
-            <Segment className="button-segment">
-                <Button compact primary onClick={this.props.addCard}>Add a card</Button>
-            </Segment>
+            <Modal className="card-modal"
+                trigger={<Button positive fluid onClick={this.handleOpen}>Add New card</Button>}
+                open={this.state.modalOpen}
+                onClose={this.handleClose}
+            >
+                <Modal.Header>Add a new card</Modal.Header>
+                <Modal.Content>
+                    <Modal.Description>
+                        <Form>
+                            <Form.Field>
+                                <label>Enter card name</label>
+                                <input autoFocus placeholder="card Name" onChange={(e: any) => this.handleInput(e.target.value)} onKeyPress={this.handleKeyPress} />
+                            </Form.Field>
+                            <Button primary type="button" onClick={this.handleClose}>Create!</Button>
+                            <Button secondary type="button" onClick={this.handleCancel}>Cancel</Button>
+                        </Form>
+                    </Modal.Description>
+                </Modal.Content>
+            </Modal>
         );
     }
 }
