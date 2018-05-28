0b79b351f987e99d49e447d01830241f2ce3531e
John Yarham <John.Yarham@red-gate.com>
2018-05-28 13:25:16 +0100

    simplify todocard

--- ClientApp/components/ToDoCard.tsx
+++ ClientApp/components/ToDoCard.tsx
@@ -10,54 +10,20 @@ interface IToDoCardProps {
     handleOpen(): void;
 }
 
-interface IToDoCardState {
-    newCardName: string;
-    newCardDescription: string;
-}
-export class ToDoCard extends React.Component<IToDoCardProps, IToDoCardState> {
-
-    public constructor() {
-        super();
-        this.state = {
-            newCardName: "",
-            newCardDescription: ""
-        }
-    }
-
-    public changeCardName = (input: string) => {
-        this.setState({ newCardName: input });
-    };
-
-    public changeCardDescription = (input: string) => {
-        this.setState({ newCardDescription: input });
-    };
-
-    public updateCardOnKeyPress = (event: any) => {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            event.target.blur();
-        }
-    };
-
-    public updateCardOnOutsideClick = () => {
-        this.props.updateCard(this.state.newCardName, this.state.newCardDescription);
-    };
-
-    public render() {
-        return (
-            <Card onClick={this.props.handleOpen} className="todo-card" as={"div"} raised>
-                <Card.Content>
-                    <Card.Header className="card-name">
-                        {this.props.cardName}
-                    </Card.Header>
-                    <Card.Description className="card-name">
-                        {this.props.cardDescription}
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <Button negative onClick={this.props.deleteCard}>Delete</Button>
-                </Card.Content>
-            </Card>
-        );
-    }
+export const ToDoCard = (props: IToDoCardProps) => {
+    return (
+        <Card onClick={props.handleOpen} className="todo-card" as={"div"} raised>
+            <Card.Content>
+                <Card.Header className="card-name">
+                    {props.cardName}
+                </Card.Header>
+                <Card.Description className="card-name">
+                    {props.cardDescription}
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button negative onClick={props.deleteCard}>Delete</Button>
+            </Card.Content>
+        </Card>
+    );
 }
