07e4d96c57489c87aee2481a512fc32987e8f379
John Yarham <yarhamjohn@gmail.com>
2018-08-08 21:41:46 +0100

    Kick off mongo server automatically on start

--- Server/DomainServices/DatabaseConnection.cs
+++ Server/DomainServices/DatabaseConnection.cs
@@ -6,9 +6,6 @@ namespace aspnetreact.Server.DomainServices
     {
         public IMongoDatabase GetDatabase(string databaseName)
         {
-            // Server must be run before this works.
-            // navigate to the folder: C:\Program Files\MongoDB\Server\3.6\bin
-            // run the command: mongod --dbpath "d:\mongodb\data"
             var connectionString = "mongodb://localhost:27017";
             var client = new MongoClient(connectionString);
             return client.GetDatabase(databaseName);
