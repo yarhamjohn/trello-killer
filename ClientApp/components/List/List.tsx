a7836b4c15369bee6f75ced4998ac3431d21eb03
John Yarham <John.Yarham@red-gate.com>
2018-08-23 20:40:37 +0100

    Further tidy

--- ClientApp/components/List/List.tsx
+++ ClientApp/components/List/List.tsx
@@ -23,16 +23,17 @@ export class TrelloList extends React.Component<ITrelloListProps, ITrelloListSta
     }
     
     render() {
+        const { list, deleteList } = this.props;
         return (
             <Segment.Group className="card-list">
                 <Segment className="header-segment">
                     <textarea
                         className="list-name"
-                        defaultValue={this.props.list.name}
+                        defaultValue={list.name}
                         onChange={(event: any) => this.changeListName(event.target.value)}
                         onKeyPress={this.updateListNameOnKeyPress}
                         onBlur={this.updateListNameOnBlur} />
-                    <Image inline onClick={this.props.deleteList} floated={"right"} src={require("./red_skull_icon.png")} className="delete-list--icon" />
+                    <Image inline onClick={deleteList} floated={"right"} src={require("./red_skull_icon.png")} className="delete-list--icon" />
                 </Segment>
                 <Segment className="cards-segment">
                     {this.getCards()}
@@ -45,12 +46,14 @@ export class TrelloList extends React.Component<ITrelloListProps, ITrelloListSta
     };
 
     getCards = () => {
-        let cards: Object[] = [];
-        for (let i = 0; i < this.props.list.cards.length; i++) {
+        const cards = this.props.list.cards;
 
-            const card = this.props.list.cards[i];
+        let cardList: Object[] = [];
+        for (let i = 0; i < cards.length; i++) {
+            const card = cards[i];
             const cardId = card.cardId;
-            cards.push(
+
+            cardList.push(
                 <UpdateCardModal key={cardId}
                     card={card}
                     deleteCard={() => this.deleteCard(cardId)}
@@ -59,29 +62,35 @@ export class TrelloList extends React.Component<ITrelloListProps, ITrelloListSta
             );
         }
 
-        return cards;
+        return cardList;
     };
 
     addCard = (cardName: string, cardDescription: string) => {
-        const newCard = { cardId: generate(), name: cardName, description: cardDescription }
-        const cards = [...this.props.list.cards, newCard];
+        const { list, updateList } = this.props;
 
-        this.props.updateList(this.props.list.name, cards);
+        const newCard = { cardId: generate(), name: cardName, description: cardDescription }
+        const cards = [...list.cards, newCard];
+
+        updateList(list.name, cards);
     };
 
     deleteCard = (cardId: string) => {
-        const filteredCards = this.props.list.cards.filter((element) => element.cardId !== cardId);
-        this.props.updateList(this.props.list.name, filteredCards);
+        const { list, updateList } = this.props;
+
+        const filteredCards = list.cards.filter((element) => element.cardId !== cardId);
+        updateList(list.name, filteredCards);
     };
 
     updateCard = (cardId: string, cardName: string, cardDescription: string) => {
-        const cards = [...this.props.list.cards];
+        const { list, updateList } = this.props;
+
+        const cards = [...list.cards];
         const cardIndex = this.getIndexToUpdate(cardId);
 
         const newCard = { cardId: cardId, name: cardName, description: cardDescription };
         cards.splice(cardIndex, 1, newCard);
 
-        this.props.updateList(this.props.list.name, cards);
+        updateList(list.name, cards);
     };
 
     changeListName = (input: string) => this.setState({ listName: input });
@@ -93,10 +102,14 @@ export class TrelloList extends React.Component<ITrelloListProps, ITrelloListSta
         }
     };
 
-    updateListNameOnBlur = () => this.props.updateList(this.state.listName, this.props.list.cards);
+    updateListNameOnBlur = () => {
+        const { list, updateList } = this.props;
+        updateList(this.state.listName, list.cards);
+    };
 
     getIndexToUpdate = (cardId: string) => {
-        const cardIds = [...this.props.list.cards].map((element) => { return element.cardId; });
+        const cards = this.props.list.cards;
+        const cardIds = [...cards].map((element) => { return element.cardId; });
         return cardIds.indexOf(cardId);
     };
 }
