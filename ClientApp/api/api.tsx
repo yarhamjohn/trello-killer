f5d887faba07ffc8719a2644384421eb562f3b61
John Yarham <John.Yarham@red-gate.com>
2018-08-23 20:07:35 +0100

    Move api calls to separate file

---
+++ ClientApp/api/api.tsx
@@ -0,0 +1,54 @@
+﻿import { IList } from "../components/Common/Interfaces";
+
+export function retrieveLists() {
+    return fetch("api/Board/RetrieveLists",
+        {
+            method: "POST",
+            headers: {
+                'Accept': "application/json",
+                'Content-Type': "application/json"
+            }
+        }
+    );
+};
+
+export function addNewList(newList: IList) {
+    fetch("api/Board/AddList", {
+        method: "POST",
+        headers: {
+            'Accept': "application/json",
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify({
+            ListId: newList.listId,
+            Name: newList.name,
+            Cards: []
+        })
+    });
+};
+
+export function modifyList(newList: IList) {
+    fetch("api/Board/ModifyList", {
+        method: "POST",
+        headers: {
+            'Accept': "application/json",
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify({
+            ListId: newList.listId,
+            Name: newList.name,
+            Cards: newList.cards
+        })
+    });
+};
+
+export function removeList(listId: string) {
+    fetch("api/Board/RemoveList", {
+        method: "POST",
+        headers: {
+            'Accept': "application/json",
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(listId)
+    });
+};
--- ClientApp/components/Board/Board.tsx
+++ ClientApp/components/Board/Board.tsx
@@ -1,9 +1,10 @@
 ﻿import * as React from "react";
+import { Loader } from "semantic-ui-react";
 import { generate } from "shortid";
 import { TrelloList } from "../List/List";
 import { AddListModal } from "../AddList/Modal";
 import { IList, ICard } from "../Common/Interfaces";
-import { Loader } from "semantic-ui-react";
+import { retrieveLists, addNewList, modifyList, removeList } from "../../api/api";
 import "./Board.css";
 
 interface ITrelloBoardState {
@@ -19,7 +20,9 @@ export class TrelloBoard extends React.Component<{}, ITrelloBoardState> {
 
     componentDidMount() {
         this.setState({ isLoading: true });
-        this.retrieveLists();
+        retrieveLists()
+            .then(response => response.json())
+            .then(data => this.setState({ isLoading: false, lists: data }));
     };
 
     render() {
@@ -41,13 +44,13 @@ export class TrelloBoard extends React.Component<{}, ITrelloBoardState> {
         for (let i = 0; i < this.state.lists.length; i++) {
 
             const list = this.state.lists[i];
-            const id = list.listId;
+            const listId = list.listId;
             lists.push(
-                <div className="board-column" key={id}>
+                <div className="board-column" key={listId}>
                     <TrelloList
                         list={list}
-                        deleteList={() => this.deleteList(id)}
-                        updateList={(listName, listCards) => { this.updateList(id, listName, listCards) }}
+                        deleteList={() => this.deleteList(listId)}
+                        updateList={(listName, listCards) => { this.updateList(listId, listName, listCards) }}
                     />
                 </div>
             );
@@ -58,89 +61,32 @@ export class TrelloBoard extends React.Component<{}, ITrelloBoardState> {
 
     addList = (listName: string) => {
         const newList: IList = { listId: generate(), name: listName, cards: [] };
-        const newLists = [...this.state.lists, newList];
+        const lists = [...this.state.lists, newList];
 
-        this.setState({ lists: newLists });
-        this.addNewList(newList);
+        this.setState({ lists: lists });
+        addNewList(newList);
     };
 
     deleteList = (listId: string) => {
-        const newLists = this.state.lists.filter((element) => {
-            return element.listId !== listId;
-        });
+        const filteredLists = this.state.lists.filter((element) => element.listId !== listId);
 
-        this.setState({ lists: newLists });
-        this.removeList(listId);
+        this.setState({ lists: filteredLists });
+        removeList(listId);
     };
 
     updateList = (listId: string, listName: string, listCards: ICard[]) => {
-        const newLists = [...this.state.lists];
+        const lists = [...this.state.lists];
         const listIndex = this.getIndexToUpdate(listId);
 
         const newList = { listId: listId, name: listName, cards: listCards };
-        console.log(listCards);
-        newLists.splice(listIndex, 1, newList);
+        lists.splice(listIndex, 1, newList);
 
-        this.setState({ lists: newLists });
-        this.modifyList(newList);
+        this.setState({ lists: lists });
+        modifyList(newList);
     };
 
     getIndexToUpdate = (listId: string) => {
-        return [...this.state.lists].map((element) => { return element.listId; }).indexOf(listId);
-    };
-
-    retrieveLists = () => {
-        fetch("api/Board/RetrieveLists",
-            {
-                method: "POST",
-                headers: {
-                    'Accept': "application/json",
-                    'Content-Type': "application/json"
-                }
-            }
-        )
-        .then(response => response.json())
-        .then(data => this.setState({ isLoading: false, lists: data }));
-    };
-
-    addNewList = (newList: IList) => {
-        fetch("api/Board/AddList", {
-            method: "POST",
-            headers: {
-                'Accept': "application/json",
-                'Content-Type': "application/json"
-            },
-            body: JSON.stringify({
-                ListId: newList.listId,
-                Name: newList.name,
-                Cards: []
-            })
-        });
-    };
-
-    modifyList = (newList: IList) => {
-        fetch("api/Board/ModifyList", {
-            method: "POST",
-            headers: {
-                'Accept': "application/json",
-                'Content-Type': "application/json"
-            },
-            body: JSON.stringify({
-                ListId: newList.listId,
-                Name: newList.name,
-                Cards: newList.cards
-            })
-        });
-    };
-
-    removeList = (listId: string) => {
-        fetch("api/Board/RemoveList", {
-            method: "POST",
-            headers: {
-                'Accept': "application/json",
-                'Content-Type': "application/json"
-            },
-            body: JSON.stringify(listId)
-        });
+        const listIds = [...this.state.lists].map((element) => { return element.listId; })
+        return listIds.indexOf(listId);
     };
 }
